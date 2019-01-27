0910a4860e7f00d8e7d85443facc4be23e7e162f
kalvin <kalvinvini1998@gmail.com>
2019-01-27 15:17:59 -0200

    twitter crawlers basics

---
+++ twitter.py
@@ -0,0 +1,74 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import requests
+import pandas as pd
+import html
+
+# get account info such as number of followers and stuff
+def twitter_profile_info(account):
+        url = "https://twitter.com/" + account
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text, 'html.parser')
+
+        tag = soup.find("li", {"class": "ProfileNav-item ProfileNav-item--tweets is-active"})
+        tweets_number = tag.find("span", {"class": "ProfileNav-value"}).get_text().strip()
+        print(tweets_number)
+
+        tag = soup.find("li", {"class": "ProfileNav-item ProfileNav-item--following"})
+        following = tag.find("span", {"class": "ProfileNav-value"}).get_text().strip()
+        print(following)
+
+        tag = soup.find("li", {"class": "ProfileNav-item ProfileNav-item--followers"})
+        followers = tag.find("span", {"class": "ProfileNav-value"}).get_text().strip()
+        print(followers)
+
+        tag = soup.find("li", {"class": "ProfileNav-item ProfileNav-item--favorites"})
+        likes = tag.find("span", {"class": "ProfileNav-value"}).get_text().strip()
+        print(likes)
+        
+        bio = soup.find("p", {"class": "ProfileHeaderCard-bio u-dir"}).get_text().strip()
+        print(bio)
+
+        join_date = soup.find("span", {"class": "ProfileHeaderCard-joinDateText js-tooltip u-dir"}).get_text().strip()
+        print(join_date)
+        
+        info = {
+                "Número de Tweets": tweets_number,
+                "Seguindo": following,
+                "Seguidores": followers,
+                "Número de Likes": likes,
+                "Bio": bio,
+                "Data de criacao": join_date
+        }
+
+        return info
+
+
+# get tweets text 
+def twitter_profile_tweets(account):
+    url = "https://twitter.com/" + account
+    source_code = requests.get(url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, 'html.parser')
+
+    tweets = [tweet.text for tweet in soup.find_all('p',{"class": "TweetTextSize"})]
+    users = [user.text for user in soup.find_all('strong', {"class": "fullname"})]
+    c = 1
+    for i in range(len(tweets)):
+        print(c, tweets[i], "\n"
+        "by ",  users[i], "\n")
+        c+=1
+
+
+
+
+# UNDONE...
+def twitter_search(word, n):
+        url = "https://twitter.com/search?q=%23"+ word + "&src=typd"
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text, 'html.parser')
+
+        tweets = [i.get_text() for i in soup.find_all("p",{"class": "TweetTextSize  js-tweet-text tweet-text"}, limit = n)]
+        print(tweets)
